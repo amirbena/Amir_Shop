dbf65af74288020c1a1e95cf368f36c3dcf1ca63
amirbena <amir12061968@gmail.com>
2020-05-21 22:02:46 +0300

    Changed index.ts and make tsc

--- server/dist/index.js
+++ server/dist/index.js
@@ -7,9 +7,10 @@ var dbconnection_1 = __importDefault(require("./db/startup/dbconnection"));
 var app_1 = __importDefault(require("./app"));
 var logger_1 = __importDefault(require("./startup/logger"));
 var intialzeRoutes_1 = __importDefault(require("./startup/intialzeRoutes"));
+var config_1 = __importDefault(require("config"));
 var server;
 dbconnection_1.default().then(function (result) {
-    var PORT = process.env.PORT || 5000;
+    var PORT = config_1.default.get("PORT") || 5000;
     server = new app_1.default(PORT, intialzeRoutes_1.default()); /* Need to change it */
     server.app.get("/", function (request, response) {
         response.send('<h1>Amir shop Application</h1>');
